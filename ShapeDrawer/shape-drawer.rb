1150ab7a4b5c0b31e6cce352c6ed8d78e519d82b
EL-S <31227083+EL-S@users.noreply.github.com>
2019-03-24 23:58:31 +1100

    Removed Comments, Fixed Bugs, Included Dependency
    Fixed some bugs with circle colours, created a method for mouse button presses and removed unneeded comments

--- ShapeDrawer/shape-drawer.rb
+++ ShapeDrawer/shape-drawer.rb
@@ -106,7 +106,6 @@ class DemoWindow < Gosu::Window
         puts("img_#{i} = Gosu::Image.new(Circle.new(#{r}))")
         puts("img_#{i}.draw(#{x-r}, #{y-r}, #{z_layer}, 1.0, 1.0, #{c_str})")
         i += 1
-        #puts("draw_triangle(#{point_1[0]}, #{point_1[1]}, #{c}, #{point_2[0]}, #{point_2[1]}, #{c}, #{point_3[0]}, #{point_3[1]}, #{c}, #{z_layer}, mode=:default)")
       end 
     when Gosu::KbZ
       @colour = Gosu::Color::RED
@@ -130,57 +129,48 @@ class DemoWindow < Gosu::Window
       @colour = Gosu::Color.argb(0xff_472a01)
       @colour_str = 'Gosu::Color.argb(0xff_472a01)'
     when Gosu::MsLeft
-      if @mode == 0
-        @new_mouse = [mouse_x.round,mouse_y.round]
-        #puts("new_x_y #{@new_mouse}")
-        width = @new_mouse[0]-@old_mouse[0]
-        height = @new_mouse[1]-@old_mouse[1]
-        dimensions = [width, height]
-        #puts("old_x_y #{@old_mouse}")
-        if @counter % 2 == 0
-          #puts("Gosu.draw_rect(#{@old_mouse[0]}, #{@old_mouse[1]}, #{width}, #{height}, Gosu::Color::BLACK, ZOrder::TOP, mode=:default)")
-          @rectangle_queue.append([@old_mouse[0], @old_mouse[1], width, height, @current_z, @colour, @colour_str])
-          @current_z += 1
-        end
-        #puts("Dimensions #{dimensions}")
-        #@rectangle_queue.append([@old_mouse[0], @old_mouse[1], width, height])
-        @old_mouse = @new_mouse
-        @counter += 1
-      elsif @mode == 1
-        @new_mouse = [mouse_x.round,mouse_y.round]
-        @current_triangle.append(@new_mouse)
-        if @counter % 3 == 0
-          #puts("draw_triangle(#{@current_triangle[0][0]}, #{@current_triangle[0][1]}, Gosu::Color::GREEN, #{@current_triangle[1][0]}, #{@current_triangle[1][1]}, Gosu::Color::GREEN, #{@current_triangle[2][0]}, #{@current_triangle[2][1]}, Gosu::Color::GREEN, ZOrder::MIDDLE, mode=:default)")
-          @current_triangle.append(@current_z)
-          @current_triangle.append(@colour)
-          @current_triangle.append(@colour_str)
-          @triangle_queue.append(@current_triangle)
-          @current_z += 1
-          @current_triangle = []
-        end
-        #puts("Dimensions #{dimensions}")
-        #@rectangle_queue.append([@old_mouse[0], @old_mouse[1], width, height])
-        @old_mouse = @new_mouse
-        @counter += 1
-      elsif @mode == 2
-        @new_mouse = [mouse_x.round,mouse_y.round]
-        #puts("new_x_y #{@new_mouse}")
-        width = @new_mouse[0]-@old_mouse[0]
-        height = @new_mouse[1]-@old_mouse[1]
-        radius = (((width**2)+(height**2))**(1.0/2)).round
-        #puts("old_x_y #{@old_mouse}")
-        if @counter % 2 == 0
-          #puts("Gosu.draw_rect(#{@old_mouse[0]}, #{@old_mouse[1]}, #{width}, #{height}, Gosu::Color::BLACK, ZOrder::TOP, mode=:default)")
-          @circle_queue.append([@old_mouse[0], @old_mouse[1], radius, @current_z, @colour, @colour_str])
-          circle_new = Gosu::Image.new(Circle.new(radius))
-          @circle_images.append(circle_new)
-          @current_z += 1
-        end
-        #puts("Dimensions #{dimensions}")
-        #@rectangle_queue.append([@old_mouse[0], @old_mouse[1], width, height])
-        @old_mouse = @new_mouse
-        @counter += 1
+      mouse_press(mouse_x, mouse_y)
+    end
+  end
+
+  def mouse_press(m_x, m_y)
+    if @mode == 0
+      @new_mouse = [m_x.round,m_y.round]
+      width = @new_mouse[0]-@old_mouse[0]
+      height = @new_mouse[1]-@old_mouse[1]
+      dimensions = [width, height]
+      if @counter % 2 == 0
+        @rectangle_queue << [@old_mouse[0], @old_mouse[1], width, height, @current_z, @colour, @colour_str]
+        @current_z += 1
       end
+      @old_mouse = @new_mouse
+      @counter += 1
+    elsif @mode == 1
+      @new_mouse = [m_x.round,m_y.round]
+      @current_triangle << @new_mouse
+      if @counter % 3 == 0
+        @current_triangle << @current_z
+        @current_triangle << @colour
+        @current_triangle << @colour_str
+        @triangle_queue << @current_triangle
+        @current_z += 1
+        @current_triangle = []
+      end
+      @old_mouse = @new_mouse
+      @counter += 1
+    elsif @mode == 2
+      @new_mouse = [m_x.round,m_y.round]
+      width = @new_mouse[0]-@old_mouse[0]
+      height = @new_mouse[1]-@old_mouse[1]
+      radius = (((width**2)+(height**2))**(1.0/2)).round
+      if @counter % 2 == 0
+        @circle_queue << [@old_mouse[0], @old_mouse[1], radius, @current_z, @colour, @colour_str]
+        circle_new = Gosu::Image.new(Circle.new(radius, 255, 255, 255))
+        @circle_images << circle_new
+        @current_z += 1
+      end
+      @old_mouse = @new_mouse
+      @counter += 1
     end
   end
 
@@ -189,26 +179,7 @@ class DemoWindow < Gosu::Window
   end
 
   def draw
-    # see www.rubydoc.info/github/gosu/gosu/Gosu/Color for colours
     draw_quad(0, 0, 0xff_ffffff, 3840, 0, 0xff_ffffff, 0, 2610, 0xff_ffffff, 3840, 2160, 0xff_ffffff, ZOrder::BACKGROUND)
-    #draw_quad(5, 10, Gosu::Color::BLUE, 200, 10, Gosu::Color::AQUA, 5, 150 Gosu::Color::FUCHSIA, 200, 150, Gosu::Color::RED, ZOrder::MIDDLE)
-    #draw_triangle(50, 50, Gosu::Color::GREEN, 100, 50, Gosu::Color::GREEN, 50, 100, Gosu::Color::GREEN, ZOrder::MIDDLE, mode=:default)
-    #draw_line(200, 200, Gosu::Color::BLACK, 350, 350, Gosu::Color::BLACK, ZOrder::TOP, mode=:default)
-    # draw_rect works a bit differently:
-    #Gosu.draw_rect(300, 200, 100, 50, Gosu::Color::BLACK, ZOrder::TOP, mode=:default)
-   
-   
-    # Circle parameter - Radius
-    #img2 = Gosu::Image.new(Circle.new(50))
-    # Image draw parameters - x, y, z, horizontal scale (use for ovals), vertical scale (use for ovals), colour
-    # Colour - use Gosu::Image::{Colour name} or .rgb({red},{green},{blue}) or .rgba({alpha}{red},{green},{blue},)
-    # Note - alpha is used for transparency.
-    # drawn as an elipse (0.5 width:)
-    #img2.draw(200, 200, ZOrder::TOP, 0.5, 1.0, Gosu::Color::BLUE)
-    # drawn as a red circle:
-    #img2.draw(300, 50, ZOrder::TOP, 1.0, 1.0, 0xff_ff0000)
-    # drawn as a red circle with transparency:
-    #img2.draw(300, 250, ZOrder::TOP, 1.0, 1.0, 0x64_ff0000)
     for rectangle in @rectangle_queue
       x = rectangle[0]
       y = rectangle[1]
