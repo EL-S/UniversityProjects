5d7ba64b51611329dbd56384a9d97abee55a9373
EL-S <31227083+EL-S@users.noreply.github.com>
2019-05-16 23:32:10 +1000

    Fixed Program
    Rewrote Functions

--- FixIt/fix_it.rb
+++ FixIt/fix_it.rb
@@ -1,31 +1,26 @@
 
 # takes a number and writes that number to a file then on each line
-# increments from zero to the number passed
+
 def write(aFile, number)
   # You might need to fix this next line:
-  aFile.puts(number)
-  index = 0
-  while (index < number)
-   aFile.puts(index.to_s)
-   index += 1
+  aFile.puts(number.to_s)
+  values = 0..number
+  for value in values
+   aFile.puts(value)
   end
 end
 
 # Read the data from the file and print out each line
 def read(aFile)
-  # Defensive programming:
   count = aFile.gets
   if (is_numeric?(count))
-    count = count.to_i
+    count = count.to_i + 1
+    count.times do
+      line = aFile.gets
+      puts "Line read: " + line
+    end
   else
-    count = 0
     puts "Error: first line of file is not a number"
-  end
-
-  index = 0
-  while (count < index)
-    line = aFile.gets
-    puts "Line read: " + line
   end
 end
 
@@ -33,13 +28,15 @@ end
 def main
   aFile = File.new("mydata.txt", "w") # open for writing
   if aFile  # if nil this test will be false
-    write(aFile, 10)
+    print "Enter a number: "
+    number = gets.chomp.to_i
+    write(aFile, number)
   else
     puts "Unable to open file to write!"
   end
   aFile.close
 
-  aFile = File.new("mydata.txt", "r") # open for writing
+  aFile = File.new("mydata.txt", "r") # open for reading
   if aFile
     read(aFile)
   end
