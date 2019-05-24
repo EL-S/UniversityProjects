386b878796dba963c36b637a012bb0edf373749b
EL-S <31227083+EL-S@users.noreply.github.com>
2019-05-24 22:43:49 +1000

    Implementation of other features
    All features from classless version are now in this one

--- CustomProgrammingAssignment/RubyMania/main_classes.rb
+++ CustomProgrammingAssignment/RubyMania/main_classes.rb
@@ -3,16 +3,31 @@ require 'gosu'
 # rewrite the parsing to get more information
 
 class Map
-    attr_accessor :map
+    attr_accessor :notes, :columns, :title, :artist, :creator, :version, :audio_filename, :audio_offset, :game_mode
 
-    def initialize(map, columns=4)
-        @map = map
+    def initialize(notes, columns, title, artist, creator, version, audio_filename, audio_offset, game_mode)
         @columns = columns
+		@notes = notes
+		@title = title
+		@artist = artist
+		@creator = creator
+		@version = version
+		@audio_filename = audio_filename
+		@audio_offset = audio_offset
+		@game_mode = game_mode
     end
 
     def print_map()
-        puts("Map has #{@columns} columns")
-        for note in @map
+        puts("Artist: #{@artist}")
+		puts("Title: #{@title}")
+		puts("Creator: #{@creator}")
+		puts("Version: #{@version}")
+		puts("Audio Filename: #{@audio_filename}")
+		puts("Audio Offset: #{@audio_offset}ms")
+		puts("Mode: #{@game_mode}")
+		puts("Columns: #{@columns}")
+		puts("Notes: #{@notes.length}")
+        for note in @notes
             note.print_data()
         end
     end
@@ -33,96 +48,160 @@ class Note
 
     def print_data()
         if @type == "hold"
-            suffix = ", end_time: #{@end_time}"
+            suffix = ", end_time: #{@end_time}ms"
         end
         puts("Note ID is #{@id}, type: #{@type}, column: #{@column}, start_time: #{@start_time}ms#{suffix}")
     end
 end
 
-def create_map(data, columns=4)
-    notes = []
+def create_map(data)
+    
+	notes_data = data['notes_data']
+	columns = data['columns']
+    column_width = data['column_width']
+    game_mode = data['game_mode']
+    title = data['title']
+    artist = data['artist']
+    creator = data['creator']
+    version = data['version']
+    audio_filename = data['audio_filename']
+    audio_offset = data['audio_offset']
+	
+	notes = []
 
-    for note_data in data
-        if note_data[1] > columns
+    for note_data in notes_data
+        if note_data[1] > columns #note exists in a column that doesn't exist, changing column number to reflect that.
             columns = note_data[1]
         end
-        if note_data[0] == "hold"
+        if note_data[0] == "hold" # a hold not with an end time
             note = Note.new(note_data[0],note_data[1],note_data[2],note_data[3])
-        else
+        else #a tap note
             note = Note.new(note_data[0],note_data[1],note_data[2])
         end
         notes << note
     end
 
-    map = Map.new(notes, columns)
+    map = Map.new(notes, columns, title, artist, creator, version, audio_filename, audio_offset, game_mode)
 
     return map
 end
 
-def read_map(file_location)
-    
-    file = File.new(file_location)
+def get_key_value_from_line(line)
+    setting_data = line.split(":")
 
-    flag = 0
-    data = []
+    setting_name = setting_data[0].strip
+    setting_value = setting_data[1..-1].join(":").strip
+    
+    return [setting_name, setting_value]
+end
+
+def process_line(data, line, flag)
+	line = line.chomp
+	if line != ""
+		if flag == 1
+			key_value = get_key_value_from_line(line)
+			
+			setting_name = key_value[0]
+			setting_value = key_value[1]
+
+			if setting_name == "AudioFilename"
+				data['audio_filename'] = setting_value
+			elsif setting_name == "AudioLeadIn"
+				data['audio_offset'] = setting_value
+			elsif setting_name == "Mode"
+				data['game_mode'] = setting_value
+			end
+		elsif flag == 2
+		elsif flag == 3
+			key_value = get_key_value_from_line(line)
+			
+			setting_name = key_value[0]
+			setting_value = key_value[1]
+
+			if setting_name == "Title"
+				data['title'] = setting_value
+			elsif setting_name == "Artist"
+				data['artist'] = setting_value
+			elsif setting_name == "Creator"
+				data['creator'] = setting_value
+			elsif setting_name == "Version"
+				data['version'] = setting_value
+			end
+		elsif flag == 4  # line should be about difficulty settings
+			key_value = get_key_value_from_line(line)
+
+			setting_name = key_value[0]
+			setting_value = key_value[1]
+
+			if setting_name == "CircleSize"
+				data['columns'] = setting_value.to_i # change columns
+				data['column_width'] = (512 / data['columns']).floor # recalculate column width
+			end
+		elsif flag == 5
+		elsif flag == 6
+		elsif flag == 7
+		elsif flag == 8  # line should be note data
+			line_data = line.split(",")
+			x = line_data[0].to_f
+			#y = line_data[1]
+			start_time = line_data[2]
+			type = line_data[3]
+			#hitsound = line_data[4]
+			column = ((x / data['column_width'])).floor
+			if column >= data['columns']
+				column = data['columns'] - 1
+			end
+			#puts(x,column_width,column)
+			if type == "128"
+				line_data_extra = line_data[5].split(":")
+				end_time = line_data_extra[0]
+				type = "hold"
+				#extras = line_data_extra[1..-1]
+				data['notes_data'] << [type, column, start_time, end_time]
+			elsif type != 8 # do not include 'spinners'
+				type = "tap"
+				data['notes_data'] << [type, column, start_time]
+			end
+		end
+		if line == "[General]"
+			flag = 1
+		elsif line == "[Editor]"
+			flag = 2
+		elsif line == "[Metadata]"
+			flag = 3
+		elsif line == "[Difficulty]"
+			flag = 4
+		elsif line == "[Events]"
+			flag = 5
+		elsif line == "[TimingPoints]"
+			flag = 6
+		elsif line == "[Colours]"
+			flag = 7
+		elsif line == "[Notes]" or line == "[HitObjects]"
+			flag = 8
+		end
+	end
+
+	return [data, flag]
+end
+
+def read_map(file_location)
+    file = File.new(file_location)
     
     columns = 4 # default columns
     column_width = (512 / columns).floor # maybe no floor
-    mode = 3 # mania
-
-    line = file.gets
-
-    while line != nil
-        line = line.chomp
-        if line != ""
-            if flag == 1  # line should be note data
-                line_data = line.split(",")
-                x = line_data[0].to_f
-                #y = line_data[1]
-                start_time = line_data[2]
-                type = line_data[3]
-                #hitsound = line_data[4]
-                column = ((x / column_width)).floor
-                if column >= columns
-                    column = columns - 1
-                end
-                #puts(x,column_width,column)
-                if type == "128"
-                    line_data_extra = line_data[5].split(":")
-                    end_time = line_data_extra[0]
-                    type = "hold"
-                    #extras = line_data_extra[1..-1]
-                    data << [type, column, start_time, end_time]
-                elsif type != 8 # do not include 'spinners'
-                    type = "tap"
-                    data << [type, column, start_time]
-                end
-            elsif flag == 2  # line should be about difficulty settings
-                setting_data = line.split(":")
-                setting_name = setting_data[0]
-                setting_value = setting_data[1]
-
-                if setting_name == "CircleSize"
-                    columns = setting_value.to_i # change columns
-                    column_width = (512 / columns).floor # recalculate column width
-                end
-            end
-            if line == "[Notes]"
-                flag = 1
-            elsif line == "[Difficulty]"
-                flag = 2
-            end
-        end
-        line = file.gets
+	
+	data = {"notes_data" => [], "columns" => 4, "column_width" => column_width, "game_mode" => 3, "title" => "", "artist" => "", "creator" => "", "version" => "", "audio_filename" => "", "audio_offset" => ""}
+	
+    flag = 0
+	
+	File.foreach(file) do |line|
+		data_from_line = process_line(data, line, flag)
+		data = data_from_line[0]
+		flag = data_from_line[1]
     end
-    #data = [["hold", 2, 1000, 1100],
-    #        ["tap", 3, 1826],
-    #        ["tap", 1, 283]
-    #    ]
 
-    #columns = 4
-
-    map = create_map(data, columns)
+    map = create_map(data)
 
     return map
 end
