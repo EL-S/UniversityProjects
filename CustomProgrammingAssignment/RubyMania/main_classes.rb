fb46332f5712b3fce85cc9b442c19c945f237a18
EL-S <31227083+EL-S@users.noreply.github.com>
2019-06-05 23:58:41 +1000

    GUI Timing
    Work In Progress

--- CustomProgrammingAssignment/RubyMania/main_classes.rb
+++ CustomProgrammingAssignment/RubyMania/main_classes.rb
@@ -1,4 +1,6 @@
 require 'gosu'
+require './file_parser'
+require 'time'
 
 # rewrite the parsing to get more information / move file parser to a secondary file
 # implement graphics
@@ -7,209 +9,81 @@ require 'gosu'
 # background slider
 # menu screen
 
-class Map
-    attr_accessor :notes, :columns, :title, :artist, :creator, :version, :audio_filename, :audio_offset, :game_mode
+WIDTH = 1920
+HEIGHT = 1080
 
-    def initialize(notes, columns, title, artist, creator, version, audio_filename, audio_offset, game_mode)
-        @columns = columns
-		@notes = notes
-		@title = title
-		@artist = artist
-		@creator = creator
-		@version = version
-		@audio_filename = audio_filename
-		@audio_offset = audio_offset
-		@game_mode = game_mode
+def read_all_maps()
+    map_locations = Dir["maps/**/*.rbm"]
+    maps = []
+
+    for map_location in map_locations
+        map = read_map(map_location)
+        maps << map
     end
 
-    def print_map()
-        puts("Artist: #{@artist}")
-		puts("Title: #{@title}")
-		puts("Creator: #{@creator}")
-		puts("Version: #{@version}")
-		puts("Audio Filename: #{@audio_filename}")
-		puts("Audio Offset: #{@audio_offset}ms")
-		puts("Mode: #{@game_mode}")
-		puts("Columns: #{@columns}")
-		puts("Notes: #{@notes.length}")
-        for note in @notes
-            note.print_data()
-        end
-    end
+    return maps
 end
 
-class Note
-    attr_accessor :type, :column, :start_time, :end_time
-    @@id = 1
+class GameWindow < Gosu::Window
+  def initialize
+    super WIDTH, HEIGHT, :fullscreen => true, :update_interval => 1
+    self.caption = "GosuMania"
+    @note_x = 0
+    @note_y = 0
+    load_maps()
+    @selected_map = @maps[0]
+    @last_frame_time = Time.now
+    @song_start_time = Time.now
+    @approach_time = 1000.0 # ms
+  end
 
-    def initialize(type, column, start_time, end_time=nil)
-        @id = @@id
-        @type = type
-        @column = column
-        @start_time = start_time
-        @end_time = end_time
-        @@id += 1
+  def update
+    @current_frame_time = Time.now
+    @time_difference = @current_frame_time - @last_frame_time
+    @note_y += (1
+    @song_time = Time.now-@song_start_time
+    @last_frame_time = @current_frame_time
+  end
+
+  def draw
+    @columns_width = (WIDTH/3)
+    @columns_offset = (WIDTH - @columns_width)/2
+    @column_width = @columns_width/@selected_map.columns
+
+    for column in (1..@selected_map.columns+1) do
+        @x = @columns_offset+((column-1)*@column_width)
+        @y = 0
+        Gosu.draw_rect(@x, @y, 1, HEIGHT, Gosu::Color::WHITE)
     end
 
-    def print_data()
-        if @type == "hold"
-            suffix = ", end_time: #{@end_time}ms"
-        end
-        puts("Note ID is #{@id}, type: #{@type}, column: #{@column}, start_time: #{@start_time}ms#{suffix}")
-    end
-end
+    Gosu.draw_rect(@columns_offset+((1-1)*@column_width), @note_y, @column_width, 100, Gosu::Color::WHITE)
+  end
 
-def create_map(data)
+  def needs_redraw? # if the fps is below  60, skip the frame
+    if @time_difference > (1.0/60) # 16.67 ms / 60 fps
+        false
+    else
+        true
+    end
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
+  end
+
+  def load_maps()
     
-	notes_data = data['notes_data']
-	columns = data['columns']
-    column_width = data['column_width']
-    game_mode = data['game_mode']
-    title = data['title']
-    artist = data['artist']
-    creator = data['creator']
-    version = data['version']
-    audio_filename = data['audio_filename']
-    audio_offset = data['audio_offset']
-	
-	notes = []
+    @maps = read_all_maps()
 
-    for note_data in notes_data
-        if note_data[1] > columns #note exists in a column that doesn't exist, changing column number to reflect that.
-            columns = note_data[1]
-        end
-        if note_data[0] == "hold" # a hold not with an end time
-            note = Note.new(note_data[0],note_data[1],note_data[2],note_data[3])
-        else #a tap note
-            note = Note.new(note_data[0],note_data[1],note_data[2])
-        end
-        notes << note
+    for map in @maps
+        map.print_info
+        puts("-"*100)
     end
 
-    map = Map.new(notes, columns, title, artist, creator, version, audio_filename, audio_offset, game_mode)
-
-    return map
+  end
 end
 
-def get_key_value_from_line(line)
-    setting_data = line.split(":")
-
-    setting_name = setting_data[0].strip
-    setting_value = setting_data[1..-1].join(":").strip
-    
-    return [setting_name, setting_value]
-end
-
-def process_line(data, line, flag) # refactor this because it is not nice, maybe have a function for each flag with an appropriate name
-	line = line.chomp
-	if line != ""
-		if flag == 1
-			key_value = get_key_value_from_line(line)
-			
-			setting_name = key_value[0]
-			setting_value = key_value[1]
-
-			if setting_name == "AudioFilename"
-				data['audio_filename'] = setting_value
-			elsif setting_name == "AudioLeadIn"
-				data['audio_offset'] = setting_value
-			elsif setting_name == "Mode"
-				data['game_mode'] = setting_value
-			end
-		elsif flag == 2
-		elsif flag == 3
-			key_value = get_key_value_from_line(line)
-			
-			setting_name = key_value[0]
-			setting_value = key_value[1]
-
-			if setting_name == "Title"
-				data['title'] = setting_value
-			elsif setting_name == "Artist"
-				data['artist'] = setting_value
-			elsif setting_name == "Creator"
-				data['creator'] = setting_value
-			elsif setting_name == "Version"
-				data['version'] = setting_value
-			end
-		elsif flag == 4  # line should be about difficulty settings
-			key_value = get_key_value_from_line(line)
-
-			setting_name = key_value[0]
-			setting_value = key_value[1]
-
-			if setting_name == "CircleSize"
-				data['columns'] = setting_value.to_i # change columns
-				data['column_width'] = (512 / data['columns']).floor # recalculate column width
-			end
-		elsif flag == 5
-		elsif flag == 6
-		elsif flag == 7
-		elsif flag == 8  # line should be note data
-			line_data = line.split(",")
-			x = line_data[0].to_f
-			#y = line_data[1]
-			start_time = line_data[2]
-			type = line_data[3]
-			#hitsound = line_data[4]
-			column = ((x / data['column_width'])).floor
-			if column >= data['columns']
-				column = data['columns'] - 1
-			end
-			#puts(x,column_width,column)
-			if type == "128"
-				line_data_extra = line_data[5].split(":")
-				end_time = line_data_extra[0]
-				type = "hold"
-				#extras = line_data_extra[1..-1]
-				data['notes_data'] << [type, column, start_time, end_time]
-			elsif type != 8 # do not include 'spinners'
-				type = "tap"
-				data['notes_data'] << [type, column, start_time]
-			end
-		end
-		if line == "[General]"
-			flag = 1
-		elsif line == "[Editor]"
-			flag = 2
-		elsif line == "[Metadata]"
-			flag = 3
-		elsif line == "[Difficulty]"
-			flag = 4
-		elsif line == "[Events]"
-			flag = 5
-		elsif line == "[TimingPoints]"
-			flag = 6
-		elsif line == "[Colours]"
-			flag = 7
-		elsif line == "[Notes]" or line == "[HitObjects]"
-			flag = 8
-		end
-	end
-
-	return [data, flag]
-end
-
-def read_map(file_location)
-    file = File.new(file_location)
-    
-    columns = 4 # default columns
-    column_width = (512 / columns).floor # maybe no floor
-	
-	data = {"notes_data" => [], "columns" => 4, "column_width" => column_width, "game_mode" => 3, "title" => "", "artist" => "", "creator" => "", "version" => "", "audio_filename" => "", "audio_offset" => ""}
-	
-    flag = 0
-	
-	File.foreach(file) do |line|
-		data_from_line = process_line(data, line, flag)
-		data = data_from_line[0]
-		flag = data_from_line[1]
-    end
-
-    map = create_map(data)
-
-    return map
-end
-
-map = read_map('sample.rbm')
-map.print_map
+window = GameWindow.new
+window.show
